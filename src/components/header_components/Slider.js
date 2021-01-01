315ddfb48db90ebd104fc1439e419e5dc6249173
Bartek Karelus <bartek.karelus@gmail.com>
2021-01-01 20:31:16 +0100

    added style to slider and images

--- src/components/header_components/Slider.js
+++ src/components/header_components/Slider.js
@@ -4,7 +4,7 @@ import styles from "../../css/header_components/slider.module.css"
 
 import img1 from "../../images/img1.jpg";
 
-import img2 from "../../images/img2.png";
+import img2 from "../../images/img2.jpg";
 
 import img3 from "../../images/img3.jpg"
 
@@ -18,28 +18,30 @@ const Slider = () => {
 
     const sliderImages = images.map((element, index) => {
         return (
-            <div key={index} className={index === counter ? styles.container_slider : styles.none}>
-                <img src={element.src} alt="obraz" />
+            <div key={index} className={index === counter ? styles.container_image : styles.none}>
+                <img className={styles.image} src={element.src} alt="obraz" />
             </div>
         )
     })
 
     const nextImage = () => {
         setCounter(counter === imagesLength ? 0 : counter + 1)
-        console.log(counter)
     }
 
     const prevImage = () => {
         setCounter(counter === 0 ? imagesLength : counter - 1)
-        console.log(counter)
     }
 
     return (
-        <>
-            <button onClick={prevImage} className={styles.prev}>&#10094;</button>
+        <div className={styles.container_slider}>
+            <div className={styles.button_prev}>
+                <button onClick={prevImage} className={styles.prev}>&#10094;</button>
+            </div>
             {sliderImages}
-            <button onClick={nextImage} className={styles.next}>&#10095;</button>
-        </>
+            <div className={styles.button_next}>
+                <button onClick={nextImage} className={styles.next}>&#10095;</button>
+            </div>
+        </div>
     )
 }
 
