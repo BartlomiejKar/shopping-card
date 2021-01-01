7b6988f236139413b326114fef707110cb833fb9
Bartek Karelus <bartek.karelus@gmail.com>
2021-01-01 18:43:46 +0100

    add images for header

--- src/components/header_components/Slider.js
+++ src/components/header_components/Slider.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from "../../css/header_components/slider.module.css"
 
+import img1 from "../../images/img1.jpg";
+
+import img2 from "../../images/img2.png";
+
+import img3 from "../../images/img3.jpg"
+
 const Slider = () => {
 
+    const images = [img1, img2, img3]
+
+    const [page, setPage] = useState(1)
+
+    const nextPage = () => {
+        console.log(page)
+        setPage(page + 1)
+
+    }
+
+    const prevPage = () => {
+        console.log(page)
+        setPage(page - 1)
+
+    }
     return (
 
         <div className={styles.container_slider}>
@@ -19,8 +40,8 @@ const Slider = () => {
                 <div>3/3</div>
                 <img src="" alt="" />
             </div>
-            <button className={styles.prev}>&#10094;</button>
-            <button className={styles.next}>&#10095;</button>
+            <button onClick={prevPage} className={styles.prev}>&#10094;</button>
+            <button onClick={nextPage} className={styles.next}>&#10095;</button>
         </div>
 
     )
