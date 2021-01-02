f24700e4cb3a51a43b821b754e17e273cb943ad5
Bartek Karelus <bartek.karelus@gmail.com>
2021-01-02 21:45:22 +0100

    add images to articles and styles

--- src/components/Content_component/ArticleData.js
+++ src/components/Content_component/ArticleData.js
@@ -1,23 +1,29 @@
+import img1 from "../../images/prezenty.jpg"
+import img2 from "../../images/obuwie.jpg"
+import img3 from "../../images/gwiazdy.jpg"
+import img4 from "../../images/smoking.jpg"
+
+
 const ArticleData = [
 
     {
         title: "Sprawdź co noszą gwiazdy",
-        img: "",
+        img: img3,
         button: "Zobacz więcej"
     },
     {
         title: "Przewodnik po prezentach",
-        img: "",
+        img: img1,
         button: "Zobacz więcej"
     },
     {
         title: "Jak dobrać idealne obuwie?",
-        img: "",
+        img: img2,
         button: "Zobacz więcej"
     },
     {
         title: "Garnitur czy smoking?",
-        img: "",
+        img: img4,
         button: "Zobacz więcej"
     }
 ]
