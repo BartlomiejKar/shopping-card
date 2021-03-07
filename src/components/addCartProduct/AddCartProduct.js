2123fa73631c1ad91e5f851bcdb34ac705532f13
Bartek Karelus <bartek.karelus@gmail.com>
2021-03-07 15:27:08 +0100

    create styles for cart component

--- src/components/addCartProduct/AddCartProduct.js
+++ src/components/addCartProduct/AddCartProduct.js
@@ -1,23 +1,38 @@
 
 import React, { useContext } from 'react';
+import styles from "./AddCartProduct.module.css"
 
 import { ProductsContext } from "../provider/providerCounter"
 
+const style = {
+    textAlign: "center",
+}
+
+
 const AddCartProduct = () => {
-    const { products } = useContext(ProductsContext);
+    const { products, deleteProduct } = useContext(ProductsContext);
     const arrayItems = products.map(({ name, img }) => {
         return (
-            <div key={name}>
-                <p>{name}</p>
-                <img src={img} alt="product" />
+
+            <div className={styles.container} key={name}>
+                <p className={styles.name} >{name}</p>
+                <img className={styles.image} src={img} alt="product" />
+                <button onClick={() => deleteProduct(name)} className={styles.button}>Remove product</button>
             </div>
         )
 
     })
+
+    const emptyCart = <div style={style}><h2>Your cart is empty</h2></div>
     return (
-        <div>
-            {arrayItems}
-        </div>
+        <>
+            <div>
+                {arrayItems.length > 0 ? arrayItems : emptyCart}
+            </div>
+            <div className={styles.totalPrice}>
+                <h3>Value:0</h3>
+            </div>
+        </>
     )
 }
 
