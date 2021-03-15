3a14c9baa732afd856bf3b7fdd6e210e8a0057fe
Bartek Karelus <bartek.karelus@gmail.com>
2021-03-15 18:58:20 +0100

    add and styled singleArticle components

--- src/components/Content_component/SingleArticle.js
+++ src/components/Content_component/SingleArticle.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import { Link } from "react-router-dom"
+
+import svgIcon from "../../icons/right-arrow.svg"
+import styles from "./SingleArticle.module.css"
+
 export const SingleArticle = (props) => {
     const { description, title, img } = props.location
     return (
-        <div>
-            <h1>
+        <div className={styles.container}>
+            <h1 className={styles.title}>
                 {title}
             </h1>
-            <img src={img} alt="" />
-            <p>{description}</p>
+            <img className={styles.image} src={img} alt="" />
+            <p className={styles.description}>{description}</p>
+            <Link className={styles.linkToBack} to="/">Powrót <img alt="" className={styles.svg_icon} src={svgIcon} /></Link>
         </div>
     )
 }
